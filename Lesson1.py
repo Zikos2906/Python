19d50050c89b0cb1a7ffe728095309b9bbd3e6d2
Zikos2906 <Bihan.Banerjee2906@gmail.com>
2025-06-06 19:27:19 +0530

    Submitting Lesson1

--- Lesson1.py
+++ Lesson1.py
@@ -1,1 +1,19 @@
 print("Hello")
+print(2012)
+
+Name = "Bihan Banerjee"
+print(Name)
+
+Marks = 99
+print(Marks)
+
+Percentile = 97.9
+print(Percentile)
+
+print("Hello, My name is ",Name)
+
+print("My English Marks are",Marks)
+
+print("My percentile for me was",Percentile)
+
+
