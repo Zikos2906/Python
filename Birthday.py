4184de6a452d08ac9d86347fb38ee4b9a8448cb1
Zikos2906 <Bihan.Banerjee2906@gmail.com>
2025-06-09 18:07:25 +0530

    UpdaTE

---
+++ Birthday.py
@@ -0,0 +1,8 @@
+Name = input("Enter your name: ")
+Year = input("Enter your Birth Year: ")
+month = input("Enter your Birth Month: ")
+Date = input("Enter your Birthday: ")
+
+age = 2025-int(Year)
+
+print(f"Happy Bithday {Name} Your birth year is on {Year} so u are {age} on {month} {Date}")
