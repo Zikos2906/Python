bcce2fb2520a2e0730bbebcea8d3c3c10f5bdf5b
Zikos2906 <Bihan.Banerjee2906@gmail.com>
2025-06-09 17:57:21 +0530

    Submitting Lesson2

---
+++ Lesson2.py
@@ -0,0 +1,41 @@
+# print("Hello World")
+
+# #Varibles
+
+# Name = "Bihan"
+# age = 12
+# school = "The Gaudium School"
+# Grade = 8
+# print(Name)
+# print("My name is ",Name,"My age is",age)
+
+# #f strings
+
+# print(f"My name is {Name} My age is {age} I go to {school} I am in {Grade}")
+
+
+# a = 5
+# b = 2
+# #Arithmic operaters
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+# print(a^b)
+
+# #Logical Operators
+
+
+# print(a<b)
+# print(a>b)
+# print(a>=b)
+# print(a<=b)
+# print(a!=b)
+# print(a==b)
+
+#input
+
+Name = input("Enter your name: ")
+age = input("Enter your age")
+print(f"Hello How are u doing {Name} you are {age} years old")
