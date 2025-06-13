f86d59183d96a36b094f89f021eeab5e36e261e5
Zikos2906 <Bihan.Banerjee2906@gmail.com>
2025-06-13 19:25:19 +0530

    Submitting Lesson3

---
+++ Lesson3.py
@@ -0,0 +1,58 @@
+# #Data Types
+
+# Name = "Bihan" #string
+# Age = 4 #integer
+# Persentile = 97.9 # float
+
+# a = 5
+# b = 3
+# print(a+b)
+
+# #Type casting
+
+# a = "6"
+# b = "9"
+
+# c = int(a)
+# d = int(b)
+# print(c+d)
+
+# e = str(c)
+# f = str(d)
+# print(e+f)
+
+#Indexing
+
+Name = "Bihan Banerjee"
+print(Name[0])
+print(Name[1])
+print(Name[2])
+print(Name[3])
+print(Name[2])
+
+#Slicing
+
+print(Name[0:5])
+print(Name[6:14])
+print(Name[2:9])
+
+
+#Function
+
+
+print(Name.upper())
+print(Name.lower())
+print(Name.title())
+
+#Coding
+school = "Codingnal"
+print(school[0:6])
+
+#Length
+print(len(Name))
+
+#Find and Replace
+print(Name.replace("Bihan","Rahul"))
+
+
+
