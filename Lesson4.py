6d8eb713af96912e18fbd903a317e9330dea69e6
Zikos2906 <Bihan.Banerjee2906@gmail.com>
2025-06-16 18:01:18 +0530

    Submitting Lesson4

---
+++ Lesson4.py
@@ -0,0 +1,40 @@
+# # math = 98
+# # english = 96
+# # social = 92
+# # hindi = 90
+# # science = 97
+
+# # sum = math+english+social+hindi+science
+# # print(sum)
+
+# # Total = 500
+# # Percentage = (sum/Total)*100
+# # print(Percentage)
+
+# student1 = 110
+# student2 = 120
+# student3 = 115
+# student4 = 135
+# student5 = 146
+# student6 = 150
+# student7 = 121
+# student8 = 113
+# student9 = 109
+# student10 = 112
+
+
+
+
+# sum = student1+student2+student3+student4+student5+student6+student7+student8+student9+student10
+# print(sum)
+# Average = sum/10
+# print(Average)
+
+
+a = input("Please Enter the amount here: ")
+print(a)
+a = int(a)
+note1 = a//100
+note2 = (a%100)//50
+print(note1)
+print(note2)
