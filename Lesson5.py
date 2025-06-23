86bac77649f4de161a2678d8018caafb580e49e8
Zikos2906 <Bihan.Banerjee2906@gmail.com>
2025-06-23 17:53:32 +0530

    Submit

--- Lesson5.py
+++ Lesson5.py
@@ -1,15 +1,28 @@
-age = 10
+# age = 10
 
-if age>=18 :
-    print("You are eligible to vote")
+# if age>=18 :
+#     print("You are eligible to vote")
 
+# else:
+#     print("You are not eligible")
+
+
+# number = 2
+
+# if number % 2 == 0:
+#     print("Even number")
+# else :
+#     print("Odd number")
+    
+
+number_of_apple = int(input("Enter total number of apples: "))
+selling_price = int(input("Selling price for each apple:"))
+buying_price = int(input("buying price for each apple:"))
+
+total_selling_price = number_of_apple*selling_price
+total_buying_price = number_of_apple*buying_price
+
+if total_selling_price > total_buying_price:
+    print(f"profit{total_selling_price-total_buying_price}ruppes")
 else:
-    print("You are not eligible")
-
-
-number = 2
-
-if number % 2 == 0:
-    print("Even number")
-else :
-    print("Odd number")
+    print(f"Loss of {total_buying_price-total_selling_price}Ruppes")
