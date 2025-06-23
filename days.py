ba3625975d78d3d13583cecb478998159280dca7
Zikos2906 <Bihan.Banerjee2906@gmail.com>
2025-06-23 18:00:50 +0530

    Submit

---
+++ days.py
@@ -0,0 +1,17 @@
+a = int(input("Enter a number between 1-7 : "))
+if a==1:
+    print("sunday")
+elif a==2:
+    print("monday")
+
+elif a==3:
+    print("Tuesday")
+
+elif a==4:
+    print("Wensday")
+elif a==5:
+    print("Thursday")
+elif a==6:
+    print("Friday")
+elif a==7:
+    print("saturday")
