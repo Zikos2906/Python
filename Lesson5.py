d978eeba3eed98f95da18eb3b8cd38c7805c6553
Zikos2906 <Bihan.Banerjee2906@gmail.com>
2025-06-23 17:38:17 +0530

    Sumbit

---
+++ Lesson5.py
@@ -0,0 +1,15 @@
+age = 10
+
+if age>=18 :
+    print("You are eligible to vote")
+
+else:
+    print("You are not eligible")
+
+
+number = 2
+
+if number % 2 == 0:
+    print("Even number")
+else :
+    print("Odd number")
