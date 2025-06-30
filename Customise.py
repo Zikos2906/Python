3f4ac11effdd7b3a196c34a9b2e8bf285cab0d7e
Zikos2906 <Bihan.Banerjee2906@gmail.com>
2025-06-30 17:59:48 +0530

    Sumbit

---
+++ Customise.py
@@ -0,0 +1,17 @@
+ride = int(input("Please Select your ride, 1.bike 2.car"))
+
+if ride == 1 :
+  Bike =   int(input("Please select what type of bike you want to choose 1.tricycle 2. gearbike 3. mountainbike "))
+  if Bike == 1:
+    print("Please use tricycle")
+  elif Bike == 2:
+    print("please use gearbike")
+  elif Bike == 3:
+    print("please use mountainbike")
+else:
+  int(input("Please select what typt of car would u like 1.Tesla 2.Kia 3.Lambo"))
+  if == 1 :
+    print("Tesla")
+   
+   
+    
