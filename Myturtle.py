16cc1ddc0fe0b672ed24cbfaa0c445ea57058717
Zikos2906 <Bihan.Banerjee2906@gmail.com>
2025-07-28 17:55:49 +0530

    Submit

---
+++ Myturtle.py
@@ -0,0 +1,26 @@
+import turtle
+
+screen = turtle.Screen()
+screen.bgcolor("Black")
+
+pen = turtle.Turtle()
+pen.color("yellow")
+pen.speed(1)
+
+#Rectangle
+pen.forward(100)
+pen.right(60)
+pen.forward(100)
+pen.right(60)
+pen.forward(100)
+pen.right(60)
+pen.forward(100)
+pen.right(60)
+pen.forward(100)
+pen.right(60)
+pen.forward(100)
+
+
+
+
+turtle.done()
--- vote.py
+++
@@ -1,5 +0,0 @@
-age = int(input("Please put your age so we can check if you are eligible for voting: "))
-if 18>=age:
-    print("You are eligible for voting")
-else:
-    print("You are not elgible for not voting")
