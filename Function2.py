b1cda5741350e786c645fd13b7258b5a131e1110
Zikos2906 <Bihan.Banerjee2906@gmail.com>
2025-08-18 18:03:15 +0530

    submit

--- Function2.py
+++ Function2.py
@@ -9,19 +9,25 @@
 # month("March")
 
 
-def add(a,b):
-    print(a+b)
-add(6,5)
+# def add(a,b):
+#     print(a+b)
+# add(6,5)
 
-def sub(a,b):
-    print(a-b)
-sub(6,5)
+# def sub(a,b):
+#     print(a-b)
+# sub(6,5)
 
-def mult(a,b):
-    print(a*b)
-mult(5,6)
+# def mult(a,b):
+#     print(a*b)
+# mult(5,6)
 
-def div(a,b):
-    print(a/b)
-div(10,2)
+# def div(a,b):
+#     print(a/b)
+# div(10,2)
+
+def hello(name="Rahul",age=10):
+    print(f"Name: {name} Age: {age}")
+hello("Bihan",13)
+hello(age = 13, name = "Bihan")
+hello(name="Bihan")
 
