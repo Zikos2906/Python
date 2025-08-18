74c1810669a125402ba5794cc05b15f81331df5a
Zikos2906 <Bihan.Banerjee2906@gmail.com>
2025-08-18 17:50:39 +0530

    submit

---
+++ Function2.py
@@ -0,0 +1,27 @@
+# def wheather():
+#     print("Monday")
+
+# wheather()
+
+# def month(date):
+#     print("The month Is "+date)
+
+# month("March")
+
+
+def add(a,b):
+    print(a+b)
+add(6,5)
+
+def sub(a,b):
+    print(a-b)
+sub(6,5)
+
+def mult(a,b):
+    print(a*b)
+mult(5,6)
+
+def div(a,b):
+    print(a/b)
+div(10,2)
+
