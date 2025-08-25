0af35fc585ba29d929e30bee45a6d250e40c2d1f
Zikos2906 <Bihan.Banerjee2906@gmail.com>
2025-08-25 18:11:40 +0530

    submit

--- Keywords.py
+++ Keywords.py
@@ -1,8 +1,16 @@
-a = input("enter a word: ")
-for i in a:
-    if(i==a):
-        print("A is found")
-        break
-    else:
-     print("A not found")
+# a = input("enter a word: ")
+# for i in a:
+#     if(i==a):
+#         print("A is found")
+#         break
+#     else:
+#      print("A not found")
     
+
+var = 10
+while var > 0:
+    var = var-1
+    if var == 5:
+        continue
+    print("/nCurrent Varible Value: ",var)
+print("/nGoodBye")
