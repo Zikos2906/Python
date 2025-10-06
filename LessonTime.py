2698d9b5ac5bb957edf35aae9e3a1d358dcd6f86
Zikos2906 <Bihan.Banerjee2906@gmail.com>
2025-10-06 17:46:03 +0530

    Submit

---
+++ LessonTime.py
@@ -0,0 +1,11 @@
+# import datetime
+#  time = datetime.datetime.now()
+# #print(time)
+
+from datetime import time , date , datetime
+
+Today = date.today()
+now = datetime.now()
+
+print("Today is ",Today)
+print("Now is ",now)
