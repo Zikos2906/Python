b9b4609a9dff69a7e21c716c8a2985664cc21d3f
Zikos2906 <Bihan.Banerjee2906@gmail.com>
2025-11-03 18:10:24 +0530

    submit

---
+++ sets.py
@@ -0,0 +1,10 @@
+my_set = {1, 2, 3}
+print(my_set)
+
+# Set of mixed data types
+my_set = {1.0, "Hello", (1, 2, 3)}
+print(my_set)
+
+# Set cannot have duplicates
+my_set = {1, 2, 3, 4, 3, 2}
+print(my_set)
